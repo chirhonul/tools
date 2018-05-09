bfdb70be2ae184e1a5fd75e550d79fbeb80879c0
Chi Nul <chinul@disroot.org>
2018-05-09 18:39:56 +0000

    Add more steps to start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,32 @@
 #
 # Script for setting up newly booted Tails instance with stuff we need.
 #
-echo "Installing packages.."
-sudo bash -c " \
-  apt-get -y update && \
-  apt-get -y install tmux"
+
+[ -e /tmp/.packages_installed_marker ] || {
+  echo "Installing packages.."
+  sudo bash -c " \
+    apt-get -y update && \
+    apt-get -y install tmux"
+  touch /tmp/.packages_installed_marker
+}
+
+[ -e /dev/mapper/unlocked ] || {
+  echo "Unlocking LUKS volume.."
+  sudo bash -c " \
+    cryptsetup open --type plain /dev/sdb3 unlocked && \
+    mount /dev/mapper/unlocked /mnt"
+}
+
+if ! gpg -k | grep -q chinul; then
+  echo "Adding GPG key.."
+  gpg --import < /mnt/keys/chinul.key
+fi
+
+if ! ssh-add -L | grep -q chirhonul; then
+  echo "Adding SSH key.."
+  ssh-add /mnt/keys/chirhonul_github0_id_rsa
+fi
+
+[ -e ~/.gitconfig ] || cp /mnt/src/.gitconfig ~/
+
+echo "Done."
