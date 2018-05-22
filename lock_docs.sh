7fb1536f1fdfdb13be5c50da6b3b615b387e1659
Chi Nul <chinul@disroot.org>
2018-05-22 09:57:10 +0000

    Harden lock_docs.sh

--- lock_docs.sh
+++ lock_docs.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+#
+# Lock the documents.
+#
 set -euo pipefail
 
-cd
+cd /tmp
 
 echo "Creating docs.tar.gz archive.."
-tar czfv docs.tar.gz ~/docs/
+tar czfv docs.tar.gz docs_clear/
 
 echo "Encrypting docs.tar.gz.."
 gpg --out ~/src/github.com/chirhonul/docs/docs.tar.gz.asc \
-    --armor --encrypt --recipient chinul ~/docs.tar.gz
+    --armor --encrypt --recipient chinul docs.tar.gz
 
 echo "Deleting docs.tar.gz.."
-srm ~/docs.tar.gz
+srm docs.tar.gz
+
+echo "Deleting /tmp/docs_clear.."
+srm -r /tmp/docs_clear
