5e2a2a138233246737873e79f94662b9b132cf7a
Chi Nul <chinul@disroot.org>
2018-05-25 15:41:44 +0000

    Add start_chrome.sh script

--- start_chrome.sh
+++ start_chrome.sh
@@ -1,2 +1,7 @@
-google-chrome-stable --proxy-server="socks5://localhost:9150" --host-resolver-rules="MAP * ~NOTFOUND , EXCLUDE localhost"
+#!/bin/sh
+#
+# Run google chrome configured to use SOCKS5 proxy.
+#
+set -eu
 
+google-chrome-stable --proxy-server="socks5://localhost:9150" --host-resolver-rules="MAP * ~NOTFOUND ,EXCLUDE localhost"
