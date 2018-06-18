c181e725a18c69df85ab1dff8b7c67aa3e7a9769
Chi Nul <chinul@disroot.org>
2018-06-18 16:10:09 +0000

    connect_monero_cli: Actually specify tor service

--- connect_monero_cli.sh
+++ connect_monero_cli.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd /mnt/keys
-torsocks /data/monero-v0.12.2.0/monero-wallet-cli --daemon-host
+TOR_SVC=$(sudo cat /var/lib/tor/hidden_service/hostname)
+torsocks /data/monero-v0.12.2.0/monero-wallet-cli --daemon-host ${TOR_SVC}
