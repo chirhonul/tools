8a0cc208dd824b729104d059f3e08ab09c9d1e09
Chi Nul <chinul@disroot.org>
2018-06-19 16:13:32 +0000

    Split up docker scripts for/without tails

--- install_docker.sh
+++ install_docker.sh
@@ -2,30 +2,18 @@
 #
 # Install docker.
 #
-# Start with:
-#   sudo adduser docker --disabled-password
-#   sudo sh -c "export PATH=$PATH:/usr/local/docker:/sbin; dockerd"
-# Pulling images via SOCKS5 proxy seems like it should work (but doesn't) with:
-#   ALL_PROXY=socks5://localhost:9150 docker run alpine echo hi
-# Unable to find image 'alpine:latest' locally
-# /usr/local/docker/docker: Error response from daemon: Get https://registry-1.docker.io/v2/: dial tcp 127.0.0.1:9150: getsockopt: connection refused.
-#
-# Another option would be to ssh forward a socket to a remote docker host:
-#   ssh -L ./mysock:/var/run/docker.sock -Nf hostname.example.com
-#   DOCKER_HOST=unix://mysock docker ps
-#
 set -eu
 
-cd /mnt/bin
-
-[ -e docker-18.03.1-ce.tgz ] || {
-  echo "Downloading docker.."
-  torify curl -vLO https://download.docker.com/linux/static/stable/x86_64/docker-18.03.1-ce.tgz
-  echo "0e245c42de8a21799ab11179a4fce43b494ce173a8a2d6567ea6825d6c5265aa docker-18.03.1-ce.tgz" | sha256sum -c -
-}
-
-if ! which docker 2>/dev/null; then
-  echo "Installing docker.."
-  sudo tar -C /usr/local -xzf docker-18.03.1-ce.tgz
-fi
+source /etc/os-release
+sudo bash -c " \
+  apt-get install -y aptitude apt-transport-https ca-certificates curl software-properties-common && \
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
+  if ! apt-key fingerprint 0EBFCD88 2>/dev/null | grep -q '9DC8 5822 9Fc7 DD38 854A E2D8 8D81 803C 0EBF CD88'; then \
+    echo 'failing; bad docker key fingerprint' && \
+    exit 1 \
+  fi && \
+  add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/'${ID}' '$(lsb_release -cs)' stable' && \
+  apt-get -y update && \
+  aptitude -y install docker-ce && \
+  adduser --ingroup docker --shell /bin/bash --disabled-password amnesia"
 
---
+++ install_docker_tails.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Install docker on tails.
+#
+# Start with:
+#   sudo adduser docker --disabled-password
+#   sudo sh -c "export PATH=$PATH:/usr/local/docker:/sbin; dockerd"
+# Pulling images via SOCKS5 proxy seems like it should work (but doesn't) with:
+#   ALL_PROXY=socks5://localhost:9150 docker run alpine echo hi
+# Unable to find image 'alpine:latest' locally
+# /usr/local/docker/docker: Error response from daemon: Get https://registry-1.docker.io/v2/: dial tcp 127.0.0.1:9150: getsockopt: connection refused.
+#
+# Another option would be to ssh forward a socket to a remote docker host:
+#   ssh -L ./mysock:/var/run/docker.sock -Nf hostname.example.com
+#   DOCKER_HOST=unix://mysock docker ps
+#
+set -eu
+
+cd /mnt/bin
+
+[ -e docker-18.03.1-ce.tgz ] || {
+  echo "Downloading docker.."
+  torify curl -vLO https://download.docker.com/linux/static/stable/x86_64/docker-18.03.1-ce.tgz
+  echo "0e245c42de8a21799ab11179a4fce43b494ce173a8a2d6567ea6825d6c5265aa docker-18.03.1-ce.tgz" | sha256sum -c -
+}
+
+if ! which docker 2>/dev/null; then
+  echo "Installing docker.."
+  sudo tar -C /usr/local -xzf docker-18.03.1-ce.tgz
+fi
+
