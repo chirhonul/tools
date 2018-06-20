22117f02ece6f1b92979f5690930e540559a2419
Chi Nul <chinul@disroot.org>
2018-06-20 19:52:18 +0000

    vnc_setup.sh: Tweak ports

--- vnc_setup.sh
+++ vnc_setup.sh
@@ -5,10 +5,10 @@
 set -eu
 
 # Set up SSH tunnel to remote host on 127.0.0.1/5901.
-ssh -L 5901:127.0.0.1:5901 -N -f -l amnesia 145.249.106.170 -p 2285
+ssh -L 8888:127.0.0.1:5901 -N -f -l amnesia 145.249.106.170 -p 2285
 
-# Whitelist the 127.0.0.1 tcp/5900 -> 5900 traffic (otherwise denied by default on Tails).
-iptables -I OUTPUT -o lo -p tcp --sport 5900 --dport 5900 -s 127.0.0.1/32 -d 127.0.0.1/32 -j ACCEPT
+# Whitelist the 127.0.0.1 tcp/8888 -> tcp/5900 traffic (otherwise denied by default on Tails).
+iptables -I OUTPUT -o lo -p tcp --sport 8888 --dport 5900 -s 127.0.0.1/32 -d 127.0.0.1/32 -j ACCEPT
 
 # On server side, start with:
 # vncserver -depth 24 -geometry 1600x1200 :0
