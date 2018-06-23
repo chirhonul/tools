33c975b5b0d5202560059d1fb69abcbde6daa5d4
Chi Nul <chinul@disroot.org>
2018-06-23 15:29:14 +0000

    install_vnc.sh: Install from apt instead

--- install_vnc.sh
+++ install_vnc.sh
@@ -4,20 +4,8 @@
 #
 set -eu
 
-source /etc/amnesia-env
-cd ${BIN_PATH}
-
-[ -e tigervnc-1.8.90.x86_64.tar.gz ] || {
-  echo "Downloading tigervnc.."
-  wget https://bintray.com/tigervnc/beta/tigervnc/1.9beta/tigervnc-1.8.90.x86_64.tar.gz
-}
-
-echo "Verifying checksum.."
-echo "078c01527b93d397f6b8312117e2f6a5bbb92fc56acb775c30f64551fe9c84ec tigervnc-1.8.90.x86_64.tar.gz | sha256sum -c -"
-
-[ -d tigervnc-1.8.90.x86_64 ] || {
-  echo "Installing tigervnc.."
-  tar xzfv tigervnc-1.8.90.x86_64.tar.gz
-}
+echo "Installing vnc server.."
+sudo apt-get -y update
+sudo apt install -y xfce4 xfce4-goodies tightvncserver
 
 echo "Done."
