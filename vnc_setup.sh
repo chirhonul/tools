dc2678b79aca964fcb423e5b5e4907ca3867e290
Chi Nul <chinul@disroot.org>
2018-06-23 15:58:55 +0000

    vnc_setup.sh: Specify correct iptables rules

--- vnc_setup.sh
+++ vnc_setup.sh
@@ -7,8 +7,9 @@ set -eu
 # Set up SSH tunnel to remote host on 127.0.0.1/5900.
 ssh -L 8888:127.0.0.1:5900 -N -l amnesia s0
 
-# Whitelist the 127.0.0.1 tcp/8888 -> tcp/5900 traffic (otherwise denied by default on Tails).
-iptables -I OUTPUT -o lo -p tcp --sport 8888 --dport 5900 -s 127.0.0.1/32 -d 127.0.0.1/32 -j ACCEPT
+# Whitelist the 127.0.0.1 -> tcp/8888 traffic (otherwise denied by default on Tails).
+iptables -I OUTPUT -o lo -p tcp --dport 8888 -s 127.0.0.1/32 -d 127.0.0.1/32 -j ACCEPT
+iptables -I OUTPUT -o lo -p tcp --dport 8889 -s 127.0.0.1/32 -d 127.0.0.1/32 -j ACCEPT
 
 # On server side, start with:
 # vncserver -depth 24 -geometry 1600x1200 :0
