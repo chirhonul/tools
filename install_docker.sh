3a9ea51439026fdbbc7a189b62166af69c524f3c
Chi Nul <chinul@disroot.org>
2018-06-23 10:30:24 +0000

    install_docker.sh: Fix subtle typos in pgp key

--- install_docker.sh
+++ install_docker.sh
@@ -14,7 +14,7 @@ source /etc/os-release
 echo "Installing docker for ${ID}.."
 apt-get install -y aptitude apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-if ! apt-key fingerprint 0EBFCD88 2>/dev/null | grep -q '9DC8 5822 9Fc7 DD38 854A E2D8 8D81 803C 0EBF CD88'
+if ! apt-key fingerprint 0EBFCD88 2>/dev/null | grep -q '9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88'; then
   echo 'failing; bad docker key fingerprint'
   exit 1
 fi
