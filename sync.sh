ea7e055ce22b1fb6bba46af81251938e4a4119bb
Chi Nul <chinul@disroot.org>
2018-06-23 16:24:08 +0000

    sync.sh: Exclude .git from sync

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/src/github.com/chirhonul/bisq
 while true; do
-  rsync -az . s0:src/github.com/chirhonul/bisq/
+  rsync -az --exclude '.git' . s0:src/github.com/chirhonul/bisq/
   sleep 5
 done
 
