48faf63d9ffba7255eb1154668b3d94a3327a484
Chi Nul <chinul@disroot.org>
2018-06-25 17:31:45 +0000

    vnc_setup.sh: Add more ports

--- vnc_setup.sh
+++ vnc_setup.sh
@@ -7,11 +7,12 @@ set -eu
 # Set up SSH tunnel to remote host on 127.0.0.1/5900.
 ssh -L 8888:127.0.0.1:5900 -N -l amnesia s0
 
-# Whitelist the 127.0.0.1 -> tcp/8888 traffic (otherwise denied by default on Tails).
+# Whitelist the 127.0.0.1 -> tcp/8888 to tcp/8890 traffic (otherwise denied by default on Tails).
 iptables -I OUTPUT -o lo -p tcp --dport 8888 -s 127.0.0.1/32 -d 127.0.0.1/32 -j ACCEPT
 iptables -I OUTPUT -o lo -p tcp --dport 8889 -s 127.0.0.1/32 -d 127.0.0.1/32 -j ACCEPT
+iptables -I OUTPUT -o lo -p tcp --dport 8890 -s 127.0.0.1/32 -d 127.0.0.1/32 -j ACCEPT
 
 # On server side, start with:
-# vncserver -depth 24 -geometry 1600x1200 :0
+# vncserver :0 -name d0 -geometry 1600x1200 -interface 127.0.0.1
 # The :0 means that tcp/5900 is used. Kill with:
 # vncserver -kill :0
