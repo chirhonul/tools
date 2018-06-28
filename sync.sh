a6be2ae606edf457a8bd87f80a46922202665897
Chi Nul <chinul@disroot.org>
2018-06-28 19:37:51 +0000

    sync.sh: Add -v

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/src/github.com/chirhonul/bisq
 while true; do
-  rsync -az --exclude '.git' . s0:src/github.com/chirhonul/bisq/
+  rsync -vaz --exclude '.git' . s0:src/github.com/chirhonul/bisq/
   sleep 5
 done
-
+# rsync -vaz --delete s2:src/github.com/chirhonul/bisq/ .
