557b5273d80fb3cc2bbd28a9ed5fb16a07c1f667
Chi Nul <chinul@disroot.org>
2018-08-01 16:59:01 +0000

    Remove sync.sh script

--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~/src/github.com/chirhonul/bisq
-while true; do
-  rsync -vaz --exclude '.git' . s0:src/github.com/chirhonul/bisq/
-  sleep 5
-done
-# rsync -vaz --delete s2:src/github.com/chirhonul/bisq/ .
