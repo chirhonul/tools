f7d7c015309ed9fb1e229fbf1879d81a7271f9f7
Chi Nul <chinul@disroot.org>
2019-06-03 05:45:01 +0000

    start_bisq.sh: Simplify script

--- start_bisq.sh
+++ start_bisq.sh
@@ -1,15 +1,13 @@
-if ! grep -q CookieAuthFile /etc/tor/torrc; then
-	# sudo bash -c "echo 'CookieAuthFile /etc/tor/control_auth_cookie' >> /etc/tor/torrc"
-	sudo systemctl restart tor@default.service
-	sleep 5
-fi
+#!/usr/bin/env bash
+
 # todo: add user to debian-tor group
 sudo chown $(id -u):$(id -g) /run/tor/control.authcookie
 if [[ ! -e /opt/Bisq/Bisq ]]; then
-    sudo dpkg -i Bisq-64bit-1.1.1.deb
+	sudo dpkg -i ~/bin/Bisq-64bit-1.1.1.deb
 fi
 if [[ ! -e ~/.local/share/Bisq ]]; then
-    cp -vr ~/docs.Bisq-backup/ ~/.local/share/Bisq/
+	mkdir -p ~/.local/share
+	cp -vr ~/docs/Bisq-backup/ ~/.local/share/Bisq/
 fi
 # /opt/Bisq/Bisq --torControlPort=9052 --torControlCookieFile=/etc/tor/control_auth_cookie
 /opt/Bisq/Bisq --torControlPort=9052 --torControlCookieFile=/run/tor/control.authcookie
